ee316ba9ec5ce34efd619c737948fed8d2aa54b3
Jameson Merkow <jmerkow@gmail.com>
2019-09-19 16:33:31 -0700

    clean up

--- models_pytorch/classification/classification.py
+++ models_pytorch/classification/classification.py
@@ -1,10 +1,11 @@
 import six
-from ..utils import Flatten, get_activation
-from ..base import Model
 import torch
 from torch import nn as nn
+
 from .classifiers import get_classifier
+from ..base import Model
 from ..encoders import get_encoder
+from ..utils import get_activation
 
 
 class ClassificationModel(Model):
@@ -16,18 +17,19 @@ class ClassificationModel(Model):
                  **kwargs):
         super().__init__()
 
-        classifiers_params = classifiers_params or [{'type': 'basic'},]
+        classifiers_params = classifiers_params or [{'type': 'basic'}, ]
         if isinstance(activation, six.string_types):
-            activation = [activation,] * len(classifiers_params)
+            activation = [activation, ] * len(classifiers_params)
 
         if isinstance(classes, six.integer_types):
-            classes = [classes,] * len(classifiers_params)
+            classes = [classes, ] * len(classifiers_params)
 
         assert len(activation) == len(classifiers_params)
 
         self.encoder = get_encoder(encoder, encoder_weights=encoder_weights, model_dir=model_dir)
         self.classifiers = nn.ModuleList([get_classifier(encoder_channels=self.encoder.out_shapes,
-                                                         classes=cl, **cp) for cp, cl in zip(classifiers_params, classes)])
+                                                         classes=cl, **cp) for cp, cl in
+                                          zip(classifiers_params, classes)])
         self.activations = [get_activation(a) for a in activation]
 
     def forward(self, x, **args):
@@ -44,4 +46,3 @@ class ClassificationModel(Model):
             outputs = self.forward(x, **args)
             scores = [act(output) for output, act in zip(outputs, self.activations)]
             return scores
-
